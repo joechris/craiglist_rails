74c856aee656a94e0e65f2113f5b3c7d30c542b7
Nicholas Wargnier <nwargnier@gmail.com>
2013-07-16 08:25:51 -0700

    Added remember token before create

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
   has_many :posts
+  before_save {self.name = name.downcase}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :token, :content, :category_id
   belongs_to :category
+  before_create :create_url_token
+
+
+  private
+    def create_url_token
+      self.token = SecureRandom.urlsafe_base64(8)
+    end
 end
