213ccd433dec47f2fd6140a9f802c798552dd549
Nicholas Wargnier <nwargnier@gmail.com>
2013-07-15 20:54:17 -0700

    Added functionality to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,28 @@
 class PostsController < ApplicationController
   def index
-    @posts = Category.find_by_name(params[:category_name]).posts
+    @category = Category.find_by_name(params[:category_name])
+    # @posts = Category.find_by_name(params[:category_name]).posts
   end
 
   def show
     @category = Category.find_by_name(params[:category_name])
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @categories = Category.all
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      flash[:success] = "Successfully create a new post"
+      redirect_to post_path(@post.category.id, @post)
+    else
+      render 'new'
+    end
+
   end
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  attr_accessible :title, :token, :content
+  attr_accessible :title, :token, :content, :category_id
   belongs_to :category
 end
